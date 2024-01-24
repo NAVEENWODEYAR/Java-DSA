70e2de7e8facdf20c213eac25a57a1350a382784
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-24 07:32:44 +0530

    Dockefile difined

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,7 @@
 #Dockerfile.,
 
 FROM openjdk:21
+RUN mkdir /usr/app/ 
+COPY target/DSA-App.jar /usr/app 
+WORKDIR /usr/app/ 
+ENTRYPOINT [ "java","-jar","DSA-App.jar" ]
--- src/main/java/com/dsa/JavaDsaApplication.java
+++ src/main/java/com/dsa/JavaDsaApplication.java
@@ -8,7 +8,6 @@ public class JavaDsaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaDsaApplication.class, args);
-		System.out.println(Thread.currentThread());
 	}
 
 }
