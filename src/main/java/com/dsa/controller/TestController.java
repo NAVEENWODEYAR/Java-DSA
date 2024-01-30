01f1150cd74ac2d36fae25ffe6fb344c693b5271
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-30 22:24:46 +0530

    new branch created

--- src/main/java/com/dsa/controller/TestController.java
+++ src/main/java/com/dsa/controller/TestController.java
@@ -1,8 +1,10 @@
 package com.dsa.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/dsa")
 public class TestController {
 
 }
