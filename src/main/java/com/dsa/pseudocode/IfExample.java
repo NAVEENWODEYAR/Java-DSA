4a3674ec6c3b433313ace476720123273f2aaefc
Naveen <naveenkrishnawodeyar@gmail.com>
2024-02-14 08:53:43 +0530

    psedocodes of basic programs

---
+++ src/main/java/com/dsa/pseudocode/IfExample.java
@@ -0,0 +1,21 @@
+
+package com.dsa.pseudocode;
+
+public class IfExample {
+    /** 
+     * start
+     * input sal
+     * if sal > 10000
+     * 	sal = sal + 20000
+     * 	output "sal"
+     * else
+     * 	sal = sal + 1000
+     * 	outprint "sal"
+     * exit
+     */
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
---
+++ src/main/java/com/dsa/pseudocode/PrimeNumber.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.dsa.pseudocode;
+
+/**
+ * 
+ */
+public class PrimeNumber {
+    /**
+     * Start
+     *  input num
+     *  while num < 2 
+     *  	if num%c=0
+     *  		output "not prime"
+     *  		exit
+     *  	c = c+1
+     *  end while
+     *  output "prime number"
+     *  exit
+     */
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
