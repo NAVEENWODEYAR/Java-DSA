087c86cfd87433b9e7a1f86ad1a08710b02076da
Naveen <naveenkrishnawodeyar@gmail.com>
2024-02-14 22:15:45 +0530

    nested if or Math.max(int a,int b);

---
+++ src/main/java/com/dsa/iterations/LargetAmong3Numbers.java
@@ -0,0 +1,23 @@
+package com.dsa.iterations;
+
+public class LargetAmong3Numbers {
+    /**
+     * find the maximum number,
+     */
+    static void maxIn3Nums(int a, int b, int c) {
+	if(a>b && a>c)
+	    System.out.println(a+" is greater than,"+b+" and "+c);
+	else if(b>c && b>a)
+	    System.out.println(b+" is greater than,"+a+" and "+c);
+	else
+	    System.out.println(c+" is greater than "+a+" and "+b);
+    }
+    
+    public static void main(String[] args) {
+	System.out.println("*********");
+	maxIn3Nums(1, 2, 3);
+	maxIn3Nums(50, 50, 50);
+	System.out.println(Math.max(4, 5));
+    }
+
+}
