a73033e6cdebd2d581bb02b82a7af24a8129ef27
Naveen <naveenkrishnawodeyar@gmail.com>
2024-02-14 22:46:31 +0530

    use modulus in occurrence programs

---
+++ src/main/java/com/dsa/pseudocode/CountNums.java
@@ -0,0 +1,24 @@
+package com.dsa.pseudocode;
+
+public class CountNums {
+    
+    /**
+     * find the occurrences of an number,
+     */
+    static void numOccurrence(int n,int m) {
+	int count=0;
+	int rem = 0;
+	while(n>0) {
+	   rem = n%10;
+	   if(rem ==m)
+	       count++;
+	   n=n/10;
+	}
+	System.out.println(count);
+    }
+
+    public static void main(String[] args) {
+	System.out.println("***********");
+	numOccurrence(1234324,4);
+    }
+}
