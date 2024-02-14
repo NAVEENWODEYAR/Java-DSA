8779162b6d520aaa3600c25b90d5e96102643585
Naveen <naveenkrishnawodeyar@gmail.com>
2024-02-14 22:55:29 +0530

    calci program

---
+++ src/main/java/com/dsa/pseudocode/Calculator.java
@@ -0,0 +1,47 @@
+package com.dsa.pseudocode;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    static void calci() {
+	Scanner sc = new Scanner(System.in);
+	int dis=0;
+	
+	while(true) {
+	    char op = sc.next().trim().charAt(0);
+	    
+	    if(op == '+' || op =='-' || op =='*' || op =='/' || op =='%') {
+		System.out.println("Enter the first number");
+		int a = sc.nextInt();
+		System.out.println("Enter the first number");
+		int b = sc.nextInt();
+		
+		if(op == '+') {
+		    dis=a+b;
+		}
+		if(op == '-') {
+		    dis=a-b;
+		}
+		if(op == '*') {
+		    dis=a*b;
+		}
+		if(op == '%') {
+		    dis=a%b;
+		}
+		if(op == '/') {
+		    if(b!=0)
+			dis=a+b;
+		}
+	    }
+	    else if(op =='x' || op =='X') {
+		System.out.println("\n Invalid choice");
+	    }
+	}
+    }
+    public static void main(String[] args) {
+	System.out.println("**********");
+	calci();
+    }
+
+}
