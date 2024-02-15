1cc7bbfc5dd1b027493cb469a2e6479dfadb5c9d
Naveen <naveenkrishnawodeyar@gmail.com>
2024-02-15 09:08:49 +0530

    Exception in thread main java.util.InputMismatchException

--- src/main/java/com/dsa/iterations/NestedSwitchCase.java
+++ src/main/java/com/dsa/iterations/NestedSwitchCase.java
@@ -9,35 +9,58 @@ public class NestedSwitchCase {
      */
     public static void main(String[] args) {
 	System.out.println("**********");
-	
 	Scanner sc = new Scanner(System.in);
-	System.out.println("Enter id and dept");
-	int empId = sc.nextInt();
-	String empDept = sc.next();
-	
-	switch (empId) {
-	case 1:
-	    System.out.println("CEO");
-	    break;
-	case 2:
-	    System.out.println("COO");
-	    break;
-	case 3:
-	    System.out.println("CTO");
-	case 4:
-	    switch (empDept) {
-	    case "IT":
-		System.out.println("Information Technology");
-		break;
-	    case "HR" :
-		System.out.println("Human Resource");
-		break;
-	    default:
-		System.out.println("Invalid choice");
-	    }
-	default:
-	    System.out.println("Enter valid option");
-	}
+	System.out.println("\nEnter student details:");
+	  char branch = sc.next().trim().charAt(0);               
+	  int collegeYear = sc.nextInt();
+	        switch( collegeYear ) {  
+	            case 1:  
+	                System.out.println("English, Maths, Science");  
+	                break;  
+	            case 2:  
+	                switch( branch )   
+	                {  
+	                    case 'C':  
+	                        System.out.println("Operating System, Java, Data Structure");  
+	                        break;  
+	                    case 'E':  
+	                        System.out.println("Micro processors, Logic switching theory");  
+	                        break;  
+	                    case 'M':  
+	                        System.out.println("Drawing, Manufacturing Machines");  
+	                        break;  
+	                }  
+	                break;  
+	            case 3:  
+	                switch( branch )   
+	                {  
+	                    case 'C':  
+	                        System.out.println("Computer Organization, MultiMedia");  
+	                        break;  
+	                    case 'E':  
+	                        System.out.println("Fundamentals of Logic Design, Microelectronics");  
+	                        break;  
+	                    case 'M':  
+	                        System.out.println("Internal Combustion Engines, Mechanical Vibration");  
+	                        break;  
+	                }  
+	                break;  
+	            case 4:  
+	                switch( branch )   
+	                {  
+	                    case 'C':  
+	                        System.out.println("Data Communication and Networks, MultiMedia");  
+	                        break;  
+	                    case 'E':  
+	                        System.out.println("Embedded System, Image Processing");  
+	                        break;  
+	                    case 'M':  
+	                        System.out.println("Production Technology, Thermal Engineering");  
+	                        break;  
+	                }  
+	                break;  
+	        }  
+	   
     }
 
 }
