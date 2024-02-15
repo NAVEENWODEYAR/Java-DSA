b8f7d3718b63ecc2283b6218c3afa527f6d01672
Naveen <naveenkrishnawodeyar@gmail.com>
2024-02-15 08:50:15 +0530

    switch advanteages over nested-ifelse

--- src/main/java/com/dsa/iterations/SwitchStatements.java
+++ src/main/java/com/dsa/iterations/SwitchStatements.java
@@ -1,20 +1,38 @@
 package com.dsa.iterations;
 
+import java.util.Scanner;
+
 public class SwitchStatements {
     /**
      * Switch conditions and statements,
      */
-    static void switchStatement(int n,int m) {
-	for(int i=0; i<=n; i++) {
-	    System.out.print(i);
-	    if(i==m)
-		break;
-	}
-    }
-    
     public static void main(String[] args) {
 	System.out.println("*****");
-	switchStatement(10, 5);
+	Scanner sc = new Scanner(System.in);
+	System.out.println("\nEnter the fruit:");
+	String fruit = sc.nextLine();
+	switch (fruit) {
+	case "Mango": 
+	    System.out.println("King,");
+	    break;
+	case "Apple": 
+	    System.out.println("Shimla,");
+	    break;
+	case "Grape": 
+	    System.out.println("Sweet & Sour,");
+	    break;
+	case "Watermelon": 
+	    System.out.println("Red,");
+	    break;
+	case "Orange": 
+	    System.out.println("Citric,");
+	    break;
+	case "Guava": 
+	    System.out.println("Nutritious,");
+	    break;
+	default:
+	    throw new IllegalArgumentException("Unexpected value: " + fruit);
+	}
     }
 
 }
--- src/main/java/com/dsa/iterations/SwitchStatements2.java
+++ src/main/java/com/dsa/iterations/SwitchStatements2.java
@@ -1,20 +1,41 @@
 package com.dsa.iterations;
 
+import java.util.Scanner;
+
 public class SwitchStatements2 {
     /**
      * Switch conditions and statements,
      */
-    static void switchStatement(int n,int m) {
-	for(int i=0; i<=n; i++) {
-	    System.out.print(i);
-	    if(i==m)
-		break;
-	}
-    }
-    
     public static void main(String[] args) {
 	System.out.println("*****");
-	switchStatement(10, 5);
+	Scanner sc = new Scanner(System.in);
+	System.out.println("\nEnter the day number to display its name:");
+	int day = sc.nextInt();
+	switch(day) {
+	case 1:
+	    System.out.println("Sunday");
+	    break;
+	case 2:
+	    System.out.println("Monday");
+	    break;
+	case 3:
+	    System.out.println("Tuesday");
+	    break;
+	case 4:
+	    System.out.println("Wednesday");
+	    break;
+	case 5:
+	    System.out.println("Thursday");
+	    break;
+	case 6:
+	    System.out.println("Friday");
+	    break;
+	case 7:
+	    System.out.println("Saturday");
+	    break;
+	default:
+	    System.out.println("Invalid option "+day);
+	}
     }
 
 }
