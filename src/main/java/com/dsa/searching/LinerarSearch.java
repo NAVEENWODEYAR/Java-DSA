52c94b553a67a6711186993ac6fbb9280e456130
Naveen <naveenkrishnawodeyar@gmail.com>
2024-02-16 09:15:01 +0530

    linear search in string

--- src/main/java/com/dsa/searching/LinerarSearch.java
+++ src/main/java/com/dsa/searching/LinerarSearch.java
@@ -2,20 +2,33 @@ package com.dsa.searching;
 
 public class LinerarSearch {
     
-    static void linearSearch(int n,int[] arr) {
+    static int linearSearch(int ele,int[] arr) {
 	
+	if(arr.length==0)
+	    return -1;
 	for(int i=0; i<arr.length; i++) {
-	    if(arr[i]==n) {
-		System.out.println("Element found at the location,"+i);
-		break;
+	    if(arr[i]==ele) {
+		return arr[i];
 	    }
-	    
 	}
+	return Integer.MAX_VALUE;
     }
     
+    static boolean linearSearch1(int[] arr,int ele) {
+	if(arr.length==0)
+	    return false;
+	for(int i=0; i<arr.length; i++) {
+	    if(arr[i]==ele)
+		return true;
+	}
+	return false;
+    }
     public static void main(String[] args) {
 	System.out.println("********");
-	linearSearch(5, new int[]{1,2,3,4,5});
+	int loc = linearSearch(5, new int[]{1,2,3,4,5});
+	System.out.println("Element found at the location "+loc);
+	boolean result = linearSearch1(new int[] {2,4,6,8,10}, 8);
+	System.out.println(result);
+	
     }
-
 }
